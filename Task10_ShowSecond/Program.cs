6139db999fbc06aa63b3cf83768e8b1cbf6a43fb
idoctorvic <loukianova@yahoo.com>
2022-11-17 16:03:37 +0300

    add Task10 Output the second digit

---
+++ Task10_ShowSecond/Program.cs
@@ -0,0 +1,6 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и 
+// на выходе показывает вторую цифру этого числа.
+Console.Write("Enter a three-digit number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(num/10%10);
