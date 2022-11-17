e2e92aef2a0f603f7540aa93d16a02df8deb61e1
idoctorvic <loukianova@yahoo.com>
2022-11-17 16:38:32 +0300

    add Task15 Weekend or not

---
+++ Task15_WeekEnd/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход цифру, 
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.Write("Enter a number between 1 and 7: ");
+int dayNum = Convert.ToInt32(Console.ReadLine());
+
+if (dayNum == 6 || dayNum == 7)
+{
+    Console.WriteLine("It's a weekend! You can relax!");
+}
+else Console.WriteLine("Today is a working day!");
